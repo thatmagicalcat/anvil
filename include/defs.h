853ea5c136efc7d20527ecb522012855ffe132f6
Diego Evaristo <di.diegoevaristo@gmail.com>
2025-07-24 12:09:58 -0300

    Definitions of feature_test_macros
    I will need this to use syscalls for definiting modification times of files and similar things

---
+++ include/defs.h
@@ -0,0 +1,11 @@
+#ifndef ANVIL_DEFINITIONS_H
+#define ANVIL_DEFINITIONS_H
+
+/*
+ * The purpose of this file is to allow for common
+ * 'feature_test_macro' definitions. 
+ */
+
+#define _POSIX_C_SOURCE 200809L
+
+#endif /* ANVIL_DEFINITIONS_H */
