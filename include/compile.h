9daec3baa71ff02975aeac84892295afd277faa2
Diego Evaristo <di.diegoevaristo@gmail.com>
2025-07-25 11:45:20 -0300

    Started working on compiling features
    Cannot yet actually solve dependencies as it is not possible to determine if a program should be compiled as a .o or not

--- include/compile.h
+++ include/compile.h
@@ -1,6 +1,8 @@
 #ifndef ANVIL_COMPILE_H
 #define ANVIL_COMPILE_H
 
+#include "tomlc17.h"
+
 // Maximum size of a command that may be executed by compile().
 #define COMPILE_CMD_MAX 4096
 
--- src/compile.c
+++ src/compile.c
@@ -10,24 +10,40 @@
 #include "tomlc17.h"
 #include "compile.h"
 
+// Helper function. Returns flags used to compile the target and sets their size o sz
+static const char *get_flags(const char *target, toml_datum_t targtab, int *sz)
+    __attribute__((nonnull));
+// Helper function. Returns full compiling command. NULL if it fails
+static const char *get_compile_cmd(const char *target, toml_datum_t targtab)
+    __attribute__((nonnull));
+
+
+// Compiles target based on the top tab of a toml file
 int compile(const char *target, toml_datum_t toptab)
 {
     int ret;
-    toml_datum_t table = toml_seek(toptab, target);
-    if (table.type != TOML_TABLE) {
+
+    toml_datum_t targtab = toml_seek(toptab, target);
+    if (targtab.type != TOML_TABLE) {
         fprintf(stderr, "%s table is either of invalid type or does not exists\n", target);
         errno = EINVAL;
         return -1;
     }
 
-    toml_datum_t deps = toml_seek(table, "dependencies");
+    toml_datum_t deps = toml_seek(targtab, "dependencies");
     if (deps.type != TOML_ARRAY) {
+        // This allows for no dependencies
+        if (deps.type == TOML_UNKNOWN) {
+            goto compile_target;
+        }
         fprintf(stderr, "%s.dependencies is either of invalid type"
                         "or does not exist\n", target);
         errno = EINVAL;
         return -1;
     }
 
+    // Solves all dependencies from a file. Aborts if operation fails
+    errno = 0;
     if (solve_dependencies(target, toptab, deps) == -1) {
         int save_errno = errno;
         fprintf(stderr, "could not solve dependencies for %s\n", target);
@@ -35,14 +51,32 @@ int compile(const char *target, toml_datum_t toptab)
         return -1;
     }
 
+compile_target:
+    const char *cmd = get_compile_cmd(target, targtab);
+    if (!cmd) {
+        return -1;
+    }
+
+    puts(cmd);
+    if (system(cmd) == -1) {
+        int save_errno = errno;
+        fprintf(stderr, "%s: %s\n", cmd, strerror(save_errno));
+        errno = save_errno;
+        return -1;
+    }
     return 0;
 }
 
+/*
+ * Solves all the dependencies of a target (recursevely calls compile() on them).
+ * TODO: implement a recursion checker to prevent against circular dependencies
+ */
 int solve_dependencies(const char *target, toml_datum_t toptab, toml_datum_t deps)
 {
-    i32 ret;
+    int ret;
 
-    for (i32 i = 0; i < deps.u.arr.size; ++i) {
+    // Iterates trough all dependencies and calls compile() on them.
+    for (int i = 0; i < deps.u.arr.size; ++i) {
         if (deps.u.arr.elem[i].type != TOML_STRING) {
             fprintf(stderr, "dependency number %i of %s is of invalid type\n", i, target);
             errno = EINVAL;
@@ -60,4 +94,55 @@ int solve_dependencies(const char *target, toml_datum_t toptab, toml_datum_t dep
     }
 
     return 0;
+}
+
+
+// Helper functions:
+
+static const char *get_compile_cmd(const char *target, toml_datum_t targtab)
+{
+
+    int flags_sz;
+    static char cmd[COMPILE_CMD_MAX];
+
+    if (!system(NULL)) {
+        fprintf(stderr, "no shell available while processing target %s\n", target);
+        return NULL;
+    }
+
+    const char *flags = get_flags(target, targtab, &flags_sz);
+    if (!flags) {
+        return NULL;
+    }
+
+    strcpy(cmd, "ccache gcc ");
+    if (strnlen(cmd, COMPILE_CMD_MAX) + strlen(target) + flags_sz + 4 > COMPILE_CMD_MAX) {
+        fprintf(stderr, "flags argument for target %s is too long\n", target);
+        errno = ERANGE;
+        return NULL;
+    }
+    
+    strcat(cmd, target);
+    strcat(cmd, ".c ");
+    strncat(cmd, flags, flags_sz);
+    return cmd;
+}
+
+static const char *get_flags(const char *target, toml_datum_t targtab, int *sz)
+{
+    toml_datum_t flags_tab = toml_seek(targtab, "flags");
+    if (flags_tab.type != TOML_STRING) {
+        // This allows to not set any flags
+        if (flags_tab.type == TOML_UNKNOWN) {
+            *sz = 0;
+            return "";
+        }
+
+        fprintf(stderr, "flags argument of %s is of invalid type\n", target);
+        errno = EINVAL;
+        return NULL;
+    }
+
+    *sz = flags_tab.u.str.len;
+    return flags_tab.u.str.ptr;
 }
